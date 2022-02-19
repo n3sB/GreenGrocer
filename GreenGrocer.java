6f74d9330b0373c781d2f71f970d9f13e3e7b884
N3sB <enesbaysl@outlook.com>
2022-02-19 14:02:51 +0300

    Update GreenGrocer.java

--- GreenGrocer.java
+++ GreenGrocer.java
@@ -2,35 +2,35 @@ import java.util.Scanner;
 
 public class GreenGrocer {
     public static void main(String[] args) {
-        
+
         Scanner scanner = new Scanner(System.in);
 
-        double pearKg, pear = 2.14, appleKg, apple = 3.67, tomatoKg, tomato = 1.11, bananaKg, banana = 0.95, eggplantKg, eggplant = 5.00;
+        double pearKg, pear = 2.14, appleKg, apple = 3.67, tomatoKg, tomato = 1.11, bananaKg, banana = 0.95, eggplantKg,
+                eggplant = 5.00;
         double cost = 0;
- 
+
         System.out.print("How many kilograms of pears ? ");
         pearKg = scanner.nextDouble();
-        cost += pearKg*pear;
+        cost += pearKg * pear;
 
         System.out.print("How many kilograms of apples ? ");
         appleKg = scanner.nextDouble();
-        cost += appleKg*apple;
+        cost += appleKg * apple;
 
         System.out.print("How many kilograms of tomatoes ? ");
         tomatoKg = scanner.nextDouble();
-        cost += tomatoKg*tomato;
+        cost += tomatoKg * tomato;
 
         System.out.print("How many kilograms of banana ? ");
         bananaKg = scanner.nextDouble();
-        cost += bananaKg*banana;
+        cost += bananaKg * banana;
 
         System.out.print("How many kilograms of eggplant ? ");
         eggplantKg = scanner.nextDouble();
-        cost += eggplantKg*eggplant;
+        cost += eggplantKg * eggplant;
         scanner.close();
 
         System.out.println("Total cost: $" + cost);
 
-       
     }
 }
